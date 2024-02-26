cf6741721656865c5268416dba1d851dc0cf5198
Sainath <71361447+sainath161@users.noreply.github.com>
2024-02-26 16:50:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,118 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-      //  sticky navbar on scroll script  //
-      if (this.scrollY > 20) {
-        $(".navbar").addClass("sticky");
-      } else {
-        $(".navbar").removeClass("sticky");
-      }
-  
-      //  scroll-up button show/hide script  //
-      if (this.scrollY > 500) {
-        $(".scroll-up-btn").addClass("show");
-      } else {
-        $(".scroll-up-btn").removeClass("show");
-      }
-    });
-  
-    //  slide-up script  //
-  
-    $(".scroll-up-btn").click(function () {
-      $("html").animate({ scrollTop: 0 });
-      //  removing smooth scroll on slide-up button click  //
-      $("html").css("scrollBehavior", "auto");
-    });
-  
-    $(".navbar .menu li a").click(function () {
-      //  Smooth scroll on Menu Items click  //
-  
-      $("html").css("scrollBehavior", "smooth");
-    });
-  
-    //  Toggle Navbar  //
-  
-    $(".menu-btn").click(function () {
-      $(".navbar .menu").toggleClass("active");
-      $(".menu-btn i").toggleClass("active");
-    });
-  
-    //  Typing Text Animation  //
-  
-    var typed = new Typed(".typing", {
-      strings: [
-        "Frontend Developer",
-        "Software Developer",
-        "Electric Vehicle Enthusiast",
-      ],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true
-    });
-  
-    var typed = new Typed(".typing-2", {
-      strings: [
-        "Frontend Developer",
-        "Software Developer",
-        "Electric Vehicle Enthusiast",
-      ],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true
-    });
-  
-    //  Owl Carousel  //
-  
-    $(".carousel").owlCarousel({
-      margin: 20,
-      loop: true,
-      autoplay: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive: {
-        0: {
-          items: 1,
-          nav: false
-        },
-        600: {
-          items: 2,
-          nav: false
-        },
-        1000: {
-          items: 3,
-          nav: false
-        }
-      }
-    });
+  $(window).scroll(function () {
+    //  sticky navbar on scroll script  //
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    //  scroll-up button show/hide script  //
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
   });
 
+  //  slide-up script  //
 
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    //  removing smooth scroll on slide-up button click  //
+    $("html").css("scrollBehavior", "auto");
+  });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const aboutSection = document.querySelector(".about");
-  
-    const observer = new IntersectionObserver(
-      function (entries) {
-        if (entries[0].isIntersecting) {
-          aboutSection.classList.add("animated");
-        }
+  $(".navbar .menu li a").click(function () {
+    //  Smooth scroll on Menu Items click  //
+
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  //  Toggle Navbar  //
+
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  //  Typing Text Animation  //
+
+  var typed = new Typed(".typing", {
+    strings: [
+      "Frontend Developer",
+      "Software Developer",
+      "Electric Vehicle Enthusiast",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Frontend Developer",
+      "Software Developer",
+      "Electric Vehicle Enthusiast",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  //  Owl Carousel  //
+
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
       },
-      { threshold: 0.5 }
-    );
-  
-    observer.observe(aboutSection);
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
   });
-  
+
+  $(".navbar .menu li a").click(function () {
+    // Remove "active" class from all navbar li items
+    $(".navbar .menu li a").removeClass("active");
+
+    // Add "active" class to the clicked navbar li item
+    $(this).addClass("active");
+
+    // Smooth scroll on Menu Items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutSection = document.querySelector(".about");
+
+  const observer = new IntersectionObserver(
+    function (entries) {
+      if (entries[0].isIntersecting) {
+        aboutSection.classList.add("animated");
+      }
+    },
+    { threshold: 0.5 }
+  );
+
+  observer.observe(aboutSection);
+});
+
 
 
 
@@ -109,11 +120,11 @@ $(document).ready(function () {
 document.addEventListener("DOMContentLoaded", function () {
   var worksSection = document.querySelector(".works");
   var observer = new IntersectionObserver(function (entries) {
-      entries.forEach(function (entry) {
-          if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
-              worksSection.classList.add("animated");
-          }
-      });
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+        worksSection.classList.add("animated");
+      }
+    });
   }, { threshold: 0.5 }); // Adding threshold: 0.5
   observer.observe(worksSection);
 });
@@ -208,43 +219,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    var contactSection = document.querySelector("#contact");
-    var title = document.querySelector("#contact .title");
-    var leftColumn = document.querySelector("#contact .column.left");
-    var rightColumn = document.querySelector("#contact .column.right");
-    var buttonArea = document.querySelector("#contact .button-area");
+document.addEventListener("DOMContentLoaded", function () {
+  var contactSection = document.querySelector("#contact");
+  var title = document.querySelector("#contact .title");
+  var leftColumn = document.querySelector("#contact .column.left");
+  var rightColumn = document.querySelector("#contact .column.right");
+  var buttonArea = document.querySelector("#contact .button-area");
 
-    var observer = new IntersectionObserver(function (entries) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          contactSection.classList.add("contact-in-view");
-          animateColumns();
-        } else {
-          contactSection.classList.remove("contact-in-view");
-        }
-      });
-    }, { threshold: 0.5 });
+  var observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        contactSection.classList.add("contact-in-view");
+        animateColumns();
+      } else {
+        contactSection.classList.remove("contact-in-view");
+      }
+    });
+  }, { threshold: 0.5 });
 
-    observer.observe(contactSection);
+  observer.observe(contactSection);
 
-    function animateColumns() {
-      title.style.opacity = 1;
-      title.style.transform = "translateY(0)";
+  function animateColumns() {
+    title.style.opacity = 1;
+    title.style.transform = "translateY(0)";
 
-      setTimeout(function () {
-        leftColumn.style.opacity = 1;
-        leftColumn.style.transform = "translateY(0)";
-      }, 500); // 1 second delay
+    setTimeout(function () {
+      leftColumn.style.opacity = 1;
+      leftColumn.style.transform = "translateY(0)";
+    }, 500); // 1 second delay
 
-      setTimeout(function () {
-        rightColumn.style.opacity = 1;
-        rightColumn.style.transform = "translateY(0)";
-      }, 1000); // 2 seconds delay
+    setTimeout(function () {
+      rightColumn.style.opacity = 1;
+      rightColumn.style.transform = "translateY(0)";
+    }, 1000); // 2 seconds delay
 
-      setTimeout(function () {
-        buttonArea.style.opacity = 1;
-        buttonArea.style.transform = "translateY(0)";
-      }, 1500); // 3 seconds delay
-    }
-  });
+    setTimeout(function () {
+      buttonArea.style.opacity = 1;
+      buttonArea.style.transform = "translateY(0)";
+    }, 1500); // 3 seconds delay
+  }
+});
